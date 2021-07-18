380e02c549d9431a529aae20e99b68c07a7420a6
StevenVallarsa <orionsjv@gmail.com>
2021-07-18 14:05:51 -0400

    Components Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
 import { CAMPSITES } from "../shared/campsites";
+import { View } from "react-native";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      selectedCampsite: null,
     };
   }
 
+  onCampsiteSelect(campsiteId) {
+    this.setState({ selectedCampsite: campsiteId });
+  }
+
   render() {
-    return <Directory campsites={this.state.campsites} />;
+    return (
+      <View style={{ flex: 1 }}>
+        <Directory
+          campsites={this.state.campsites}
+          onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+        />
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === this.state.selectedCampsite
+            )[0]
+          }
+        />
+      </View>
+    );
   }
 }
 
