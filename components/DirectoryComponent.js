9ebfa4c06e0e5e6918eb9b3ca10a05f5b7256bcc
StevenVallarsa <orionsjv@gmail.com>
2021-07-19 13:09:00 -0400

    ???

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,25 +1,40 @@
 import React, { Component } from "react";
 import { FlatList } from "react-native";
 import { ListItem } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
-function Directory(props) {
-  const renderDirectoryItem = ({ item }) => {
+class Directory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+    };
+  }
+
+  static navigationOptions = {
+    title: "Directory",
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+    const renderDirectoryItem = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })}
+          leftAvatar={{ source: require("./images/react-lake.jpg") }}
+        />
+      );
+    };
     return (
-      <ListItem
-        title={item.name}
-        subtitle={item.description}
-        onPress={() => props.onPress(item.id)}
-        leftAvatar={{ source: require("./images/react-lake.jpg") }}
+      <FlatList
+        data={this.state.campsites}
+        renderItem={renderDirectoryItem}
+        keyExtractor={(item) => item.id.toString()}
       />
     );
-  };
-  return (
-    <FlatList
-      data={props.campsites}
-      renderItem={renderDirectoryItem}
-      keyExtractor={(item) => item.id.toString()}
-    />
-  );
+  }
 }
 
 export default Directory;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,36 +1,71 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
-import { CAMPSITES } from "../shared/campsites";
-import { View } from "react-native";
+import Constants from "expo-constants";
+import { View, Platform } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { createAppContainer } from "react-navigation";
+
+const DirectoryNavigator = createStackNavigator(
+  {
+    Directory: { screen: Directory },
+    CampsiteInfo: { screen: CampsiteInfo },
+  },
+  {
+    initialRouteName: "Directory",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeNavigator },
+    Directory: { screen: DirectoryNavigator },
+  },
+  {
+    drawerBackgroundColor: "#CEC8FF",
+  }
+);
+
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES,
-      selectedCampsite: null,
-    };
-  }
-
-  onCampsiteSelect(campsiteId) {
-    this.setState({ selectedCampsite: campsiteId });
-  }
-
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Directory
-          campsites={this.state.campsites}
-          onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
-        />
-        <CampsiteInfo
-          campsite={
-            this.state.campsites.filter(
-              (campsite) => campsite.id === this.state.selectedCampsite
-            )[0]
-          }
-        />
+      <View
+        style={{
+          flex: 1,
+          paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+        }}
+      >
+        <AppNavigator />
       </View>
     );
   }
