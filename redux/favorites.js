3c80ca1b0a4414099f86cd04657f849a16d8884d
StevenVallarsa <orionsjv@gmail.com>
2021-08-02 19:17:48 -0400

    Swipe and Delete Favorites

--- redux/favorites.js
+++ redux/favorites.js
@@ -4,10 +4,15 @@ export const favorites = (state = [], action) => {
   switch (action.type) {
     case ActionTypes.ADD_FAVORITE:
       if (state.includes(action.payload)) {
+        /*  Instead of just returning state, if the user selected the
+         *  favorite button again, this will remove the campground
+         *  from the favorites list
+         */
         return state.filter((id) => id !== action.payload);
       }
       return state.concat(action.payload);
-
+    case ActionTypes.DELETE_FAVORITE:
+      return state.filter((id) => id !== action.payload);
     default:
       return state;
   }
